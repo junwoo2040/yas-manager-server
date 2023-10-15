bde7d70740d2aac8a525512f7e3c2be2f469ffef
junwoo2040 <24baekj@isyedu.org>
2023-10-15 12:03:38 +0630

    Add Pothos for end-to-end type safety

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,36 @@
+// Path alias setup
 import "./paths";
 
+// Express server & CORS config
 import express from "express";
 import cors, { CorsOptions } from "cors";
 
+// Routers
 import graphQLRouter from "@routes/graphql";
 import playgroundRouter from "@routes/playground";
 
+// Environment variables
 import dotenv from "dotenv";
-
 dotenv.config();
-
-const app = express();
 const port = process.env.PORT;
 
-const allowedOrigins = ["http://localhost:8000", "http://localhost:5173"];
+// Create new express server
+const app = express();
 
+// Create CORS config
+const allowedOrigins = ["http://localhost:8000", "http://localhost:5173"];
 const options: CorsOptions = {
-    origin: allowedOrigins,
+  origin: allowedOrigins,
 };
 
+// Use created CORS policy
 app.use(cors(options));
 
+// Add routes
 app.use("/graphql", graphQLRouter);
 app.use("/playground", playgroundRouter);
 
+// Run server
 app.listen(port, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
--- src/routes/graphql.ts
+++ src/routes/graphql.ts
@@ -1,8 +1,17 @@
 import { Router } from "express";
 
-import { GraphQLObjectType, GraphQLSchema } from "graphql";
+// import { GraphQLObjectType, GraphQLSchema } from "graphql";
 import { createHandler } from "graphql-http/lib/use/http";
 
+// Prisma
+import { prisma } from "@utils/index";
+
+// Pothos
+import SchemaBuilder from "@pothos/core";
+import PrismaPlugin from "@pothos/plugin-prisma";
+import type PrismaTypes from "@pothos/plugin-prisma/generated";
+
+// Import models
 import { userQuery, userMutation } from "@models/user";
 import { eventMutation, eventQuery } from "@models/event";
 import { taskMutation, taskQuery } from "@models/task";
@@ -10,34 +19,60 @@ import { shiftMutation, shiftQuery } from "@models/shift";
 import { productMutation, productQuery } from "@models/product";
 import { recordMutation, recordQuery } from "@models/records";
 
+const builder = new SchemaBuilder<{ PrismaTypes: PrismaTypes }>({
+  plugins: [PrismaPlugin],
+  prisma: {
+    client: prisma,
+  },
+});
+
+builder.prismaObject("User", {});
+
+builder.queryType({
+  fields: (t) => ({
+    hello: t.string({
+      args: {
+        name: t.arg.string(),
+      },
+      resolve: (parent, { name }) => `Hello, ${name || "World"}`,
+    }),
+  }),
+});
+
+/*
 const rootQuery = new GraphQLObjectType({
-    name: "Query",
-    fields: {
-        ...userQuery,
-        ...eventQuery,
-        ...taskQuery,
-        ...shiftQuery,
-        ...productQuery,
-        ...recordQuery,
-    },
+  name: "Query",
+  fields: {
+    ...userQuery,
+    ...eventQuery,
+    ...taskQuery,
+    ...shiftQuery,
+    ...productQuery,
+    ...recordQuery,
+  },
 });
 
 const rootMutation = new GraphQLObjectType({
-    name: "Mutation",
-    fields: {
-        ...userMutation,
-        ...eventMutation,
-        ...taskMutation,
-        ...shiftMutation,
-        ...productMutation,
-        ...recordMutation,
-    },
+  name: "Mutation",
+  fields: {
+    ...userMutation,
+    ...eventMutation,
+    ...taskMutation,
+    ...shiftMutation,
+    ...productMutation,
+    ...recordMutation,
+  },
 });
+*/
 
+/*
 const schema = new GraphQLSchema({
-    query: rootQuery,
-    mutation: rootMutation,
+  query: rootQuery,
+  mutation: rootMutation,
 });
+*/
+
+const schema = builder.toSchema();
 
 const router = Router();
 router.all("/", createHandler({ schema }));
